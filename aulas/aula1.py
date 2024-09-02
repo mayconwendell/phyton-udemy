888fad8a8196c6dae7533c362d8883a2b5dd954c
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-09-02 18:35:35 -0300

    att

---
+++ aulas/aula1.py
@@ -0,0 +1,17 @@
+camisas=50
+calcas=30
+sapatos=43
+
+string="bob"
+string="ana"
+string="ted"
+
+if(camisas > calcas):
+    calcas=calcas+camisas-calcas
+
+if(camisas+calcas+sapatos > 100):
+    bob=""
+else:
+    ted=""    
+
+print('Hello, World')
