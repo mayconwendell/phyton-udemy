a0b495c7f5daa8b31d6fdff3dc0d607e4e0b8912
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-09-05 14:08:26 -0300

    att

--- aulas/aula1.py
+++ aulas/aula1.py
@@ -1,17 +1,3 @@
-camisas=50
-calcas=30
-sapatos=43
-
-funcionario1="bob"
-funcionario2="ana"
-funcionario3="ted"
-
-if(camisas > calcas):
-    print(calcas= 30 + 50 - 30)
-
-if(camisas+calcas+sapatos > 100):
-    print(demita="funcionario1")
-else:
-    print(demita="funcionario3") 
-
-print('Hello, World')
+print(12, 34, sep="-")
+print(56, 78, sep="-")
+print(14, 9, 2005, sep="-")
