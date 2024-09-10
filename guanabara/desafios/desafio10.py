22c616892f5326ba3e8f64512af1cc30b508f640
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-09-10 12:17:08 -0300

    att

--- guanabara/desafios/desafio10.py
+++ guanabara/desafios/desafio10.py
@@ -5,10 +5,12 @@ reais = float(input("Quantos reais você tem:"))
 
 dolar = 3.27
 
-if reais ==3.27:
+euro = 6.16
+
+if reais ==dolar:
     print("Você pode comprar US$ 1.00.")
 
-elif reais >=3.27:
-    print("Você pode comprar US$ {:.10}, e sobra {:.2}".format(reais/dolar, reais%dolar)) 
+elif reais >=dolar:
+    print("Você pode comprar US$ {:.2f}, e sobra {:.2f}".format(reais/dolar, reais%dolar))
 else:
     print("Você não tem saldo suficiente para adiquirir dólares")
--- guanabara/desafios/desafio11.py
+++ guanabara/desafios/desafio11.py
@@ -2,11 +2,11 @@
 #calcule a sua área e a quantidade de tinta necessária para pintar-la
 #sabendo que cada litro de tinta, pinta uma área de 2m quadrados
 
-largura = int(input("Digite a largura da sua parede: "))
-altura = int(input("Digite a altura da sua parede: "))
+largura = float(input("Digite a largura da sua parede: "))
+altura = float(input("Digite a altura da sua parede: "))
 
 area = largura*altura
 tinta = 2
 
-print("A área da sua parede é de {}, você ".format(area), end="")
-print("precisa de {} litros de tinta para pinta-la".format(area/tinta))
+print("A área da sua parede é de {} m²".format(area))
+print("Você precisa de {} litros de tinta para pinta-la".format(area/tinta))
