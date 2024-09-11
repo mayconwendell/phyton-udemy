d841e02ae21c1efe1dfe9b128166f36e2cd4e367
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-09-11 09:45:03 -0300

    att

---
+++ guanabara/aulas/manipulando-texto.py
@@ -0,0 +1,25 @@
+frase = "Uma vez Flamengo sempre Flamengo"
+
+print(frase)
+print(frase[:13])
+print(frase[2:10])
+print(frase[:15:4])
+print(frase[::2])
+
+print(frase.count('a'))
+
+print(len(frase))
+
+print(frase.count())
+
+
+
+
+
+
+
+
+#é possível usar """ para pedir para printar um texto grande
+print("""aisjdalsjdlkajsdlkajslaisjdalsjdlkajsdlkajslaisjdalsjdlkajs
+aisjdalsjdlkajsdlkajslaisjdalsjdlkajsdlkajslaisjdalsjdlkajs
+aisjdalsjdlkajsdlkajslaisjdalsjdlkajsdlkajslaisjdalsjdlkajs""")
--- guanabara/desafios/desafio21.py
+++ guanabara/desafios/desafio21.py
@@ -1,2 +1,5 @@
 #Faça um programa em python que abra e reproduza o áudio de um
 #arquivo MP3.
+
+
+
