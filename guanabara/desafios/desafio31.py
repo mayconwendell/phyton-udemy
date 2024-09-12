cddf37a198d9848f76b13d81c7ba8b8562335980
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-09-12 15:00:45 -0300

    att

---
+++ guanabara/desafios/desafio31.py
@@ -0,0 +1,3 @@
+#Desenvolva um programa que pergunte a distância de uma viagem em Km.
+#Calcule o preço da passagem, cobrando R$0,50 po km para viagens de
+#até 200Km a R$0,45 para viagens mais longas.
---
+++ guanabara/desafios/desafio35.py
@@ -0,0 +1,2 @@
+#Desenvolva um programa que leia o comprimento de três retas e digas
+#ao usuário se elas podem ou não formar um triângulo
