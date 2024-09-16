5778f90972312de3d37e9b776eeee3ceadd7cb08
Maycon Wendell <164102300+mayconwendell@users.noreply.github.com>
2024-09-16 17:15:08 -0300

    att

--- guanabara/desafios/desafio29.py
+++ guanabara/desafios/desafio29.py
@@ -6,9 +6,9 @@
 
 km = float(input("Digite quantos Km/h você estava dirigindo: "))
 
-multa = 7 * (km%80)
+multa = (km - 80) * 7 
 
-if km >80:
-    print(f'Você estava andando à {km} Km/h, você foi multado em R${multa:.1f}')
+if km > 80:
+    print(f'Você estava andando à {km} Km/h, você foi multado em R${multa:.2f}')
 else:
-    print(f'Você estava andando à {km} Km/h, continua se policiando no volante!')
+    print(f'Você estava andando à {km} Km/h, continue se policiando no volante!')
